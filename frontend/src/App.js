4c025da7c5596b86caf0bdd8100c1e205510a504
Chinmay Hegde <cod1.1backup@gmail.com>
2024-12-16 23:43:18 +0530

    Modified App.js and added react router dom routings with error page, unauthorized page, about page, admin panel page, package page, and packages page. Created the pages for each route.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,84 @@
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import ErrorPage from "./Pages/ErrorPage";
+import HomePage from "./Pages/HomePage";
+import Footer from "./Components/Footer";
+import UnauthenticatedPage from "./Pages/UnauthenticatedPage";
+import UnauthorizedPage from "./Pages/UnauthorizedPage";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import AdminPanel from "./Pages/AdminPanel";
+import About from "./Pages/About";
+import Package from "./Pages/Package";
+import Packages from "./Pages/Packages";
+
+
+
+const Layout = () => (
+  <div className="flex flex-col min-h-screen bg-gray-100">
+    <NavBar />
+    <main className="flex-grow">
+      <Outlet /> {/* This renders the matched child route */}
+    </main>
+    <Footer />
+  </div>
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />, // Use Layout to wrap routes with NavBar
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/package/:packageId",
+        element: <Package />,
+      },
+      {
+        path: "/packages",
+        element: <Packages />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <AdminPanel />,
+  },
+  {
+    path: "/notAuthorized",
+    element: <UnauthorizedPage />,
+  },
+  {
+    path: "/unauthenticated",
+    element : <UnauthenticatedPage/>
+  }
+]);
+
 function App() {
   return (
 <>
-Travel Agency
-</>
+      <RouterProvider router={router} />;     
+  </>
+
   );
 }
 
