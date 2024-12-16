593c4495ccf7cbcd2c28d58d655478e6b61f5003
Chinmay Hegde <cod1.1backup@gmail.com>
2024-12-17 00:05:41 +0530

    Enhanced Navbar and changed the travel agency name to TravelGo.

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -12,9 +12,9 @@ const Footer = () => {
           {/* Logo and Links */}
           <div className="flex flex-col items-center md:items-start">
             <Link to="/">
-              <h1 className="text-2xl font-bold mb-2 mt-3 md:mt">Travel Agency</h1>
+              <h1 className="text-2xl font-bold mb-2 mt-3 md:mt">TravelGo</h1>
             </Link>
-            <p className="text-sm mb-4">Your trusted Travel Agency Booking System</p>
+            <p className="text-sm mb-4">Your trusted Travel Agency Booking System </p>
             <nav className="flex flex-col md:flex-row gap-4">
               <Link to="/" className="hover:text-cyan-500 transition ease-in-out">Home</Link>
               <Link to="/packages" className="hover:text-cyan-500 transition ease-in-out">Packages</Link>
@@ -31,7 +31,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="text-center mt-8 border-t border-gray-300 pt-4">
-          <p className="text-sm">&copy; {new Date().getFullYear()} Travel Agency. All rights reserved.</p>
+          <p className="text-sm">&copy; {new Date().getFullYear()} TravelGo. All rights reserved.</p>
         </div>
       </div>
     </footer>
