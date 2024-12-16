7a518e46ac2be154858939800974fd44a2ceec76
Chinmay Hegde <cod1.1backup@gmail.com>
2024-12-16 13:16:46 +0530

    Added routes for admin and packages with controllers.

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import packagesController from '../controllers/packagesController.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('Admin Api');
 })
+
+router.post("/packages", packagesController.postPackages); 
+router.put("/packages", packagesController.updatePackages); 
+router.delete("/packages", packagesController.deletePackages); 
 
 export default router;
--- backend/routes/packagesRoutes.js
+++ backend/routes/packagesRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import packagesController from '../controllers/packagesController.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Packages Api');
-})
+// router.get('/', (req, res) => {
+//     res.send('Packages Api');
+// })
+
+router.get("/", packagesController.getPackages); 
 
 export default router;
