d7de70ea2a4a2262b5754b54daf23af9a0f87e00
Chinmay Hegde <cod1.1backup@gmail.com>
2024-12-16 12:52:00 +0530

    Added new 3 routes for admin, booking and packages and modified server.js.

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,22 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import "dotenv/config";
 import connectDB from './config/db.js';
+import packagesRoutes from './routes/packagesRoutes.js';
+import bookingRoutes from './routes/bookingRoutes.js';
+import adminRoutes from './routes/adminRoutes.js';
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
 
+app.use('/api/packages', packagesRoutes);
+app.use('/api/bookings', bookingRoutes);
+app.use('/api/admin', adminRoutes);
+
 app.get('/', (req,res)=>{
-    res.send('Hello World');
+    res.send('Backend is running ^^');
 })
+
 
 let port = process.env.PORT || 4000;
 
